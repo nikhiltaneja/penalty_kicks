c4948ed4ec3b4f5aca629931b273686b319bd54d
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-11-22 16:50:57 -0700

    Add test for goalie blocking

--- penalty_kick_test.rb
+++ penalty_kick_test.rb
@@ -9,11 +9,13 @@ class PenaltyKickTest < MiniTest::Test
   def test_goalie_can_guess
     goalie = Goalie.new
     goalie.guess("L")
+    assert goalie
   end
 
   def test_player_can_kick
     player = Player.new
     player.kick("L")
+    assert player
   end
 
   def test_player_can_score
@@ -23,5 +25,14 @@ class PenaltyKickTest < MiniTest::Test
     goalie.guess("R")
     player.kick("L")
     assert_equal "Goal!!!", match.score
+  end
+
+  def test_goalie_can_block
+    goalie = Goalie.new
+    player = Player.new
+    match = Match.new(goalie, player)
+    goalie.guess("R")
+    player.kick("R")
+    assert_equal "Blocked", match.score
   end
 end
